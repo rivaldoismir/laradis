ac320a20b02b0d29b4d1bf51a01042ae9469bd2f
Rivaldo Ismir <rivaldoismira1@gmail.com>
2022-05-04 09:14:41 +0700

    init 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // Redis::set('world', 'hello');
+    // return Redis::get('world');
+
     return view('welcome');
 });
+
+
+Route::get('/article/{id}', function ($id) {
+    $views = Redis::get("article.{$id}.views");
+    return "Article dengan id {$id} memiliki {$views} viewer";
+});
+
+Route::get('/article/{id}/visit', function ($id) {
+    $views = Redis::incr("article.{$id}.views");
+    return redirect()->back();
+});
+
+
+// more command in https://redis.io/commands/
+// 127.0.0.1:6379> ZA
+// (integer) 1       
+// 127.0.0.1:6379> ZA
+// (integer) 1       
+// 127.0.0.1:6379> ZR
+// 1) "belajar-larave
+// 2) "belajar-php"  
+// 127.0.0.1:6379> ZR
+// 1) "belajar-larave
+// 2) "1"            
+// 3) "belajar-php"  
+// 4) "1"            
+// 127.0.0.1:6379> ZI
+// "11"              
+// 127.0.0.1:6379> ZR
+// 1) "belajar-php"  
+// 2) "1"            
+// 3) "belajar-larave
+// 4) "11"           
+// 127.0.0.1:6379> ZR
+// 1) "belajar-larave
+// 2) "11"           
+// 3) "belajar-php"  
+// 4) "1"            
+// 127.0.0.1:6379> ZC
+// (integer) 2       
